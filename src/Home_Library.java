2574d53e0d9b02e6426555623824a149ff6861e4
Borislove <abirme@yandex.ru>
2020-12-24 01:36:06 +0300

    дополнительные переменные

--- src/Home_Library.java
+++ src/Home_Library.java
@@ -6,21 +6,21 @@ public class Home_Library {
     private String first_name; //имя
     private String middle_name; //отчество?
 
-    private String genre; //жанр
+     String genre; //жанр
 
-    private int nop; //number of pages //количество стр.
-    private int yop; //year_of_publication //год издания
-    private String ph; //publishing house //издательство
+     int number_of_pages; //number of pages //количество стр.
+     int year_of_publication; //year_of_publication //год издания
+     String publiching_house; //publishing house //издательство
 
-    private String crust; //корка?, переплёт
+     String crust; //корка?, переплёт
 
-    public Home_Library(String last_name, String first_name, String middle_name, String genre, int nop, int yop, String ph) {
+/*    public Home_Library(String last_name, String first_name, String middle_name, String genre, int number_of_pages, int yop, String ph) {
         this.last_name = last_name;
         this.first_name = first_name;
         this.middle_name = middle_name;
         this.genre = genre;
-        this.nop = nop;
+        this.number_of_pages = number_of_pages;
         this.yop = yop;
         this.ph = ph;
-    }
+    }*/
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+//при воде ф.и.о не должно быть символов
+//1 буква ф, должна быть большой
+//и.о , инициалы, если одна бука то ставить точку
 public class Main {
 
     //public static String sc = new Scanner(System.in).nextLine();
@@ -11,6 +14,7 @@ public class Main {
         //    System.out.println(new Home_Library("Pupkin", "Vasya", "Sergeevich", "philosophy", 777, 2011, "Dialectic"));
 
         Author av = new Author();
+        Home_Library hl = new Home_Library();
 
         System.out.println("Имя");
         av.first_name = new Scanner(System.in).nextLine();
@@ -19,16 +23,29 @@ public class Main {
         System.out.println("Отчество");
         av.middle_name = new Scanner(System.in).nextLine();
 
+        System.out.println("Жанр: ");
+        hl.genre = new Scanner(System.in).nextLine();
 
-        System.out.print("Имя: ");
+        System.out.println("Кол-во страниц: ");
+        hl.number_of_pages = new Scanner(System.in).nextInt();  // INTEGER!
+
+        System.out.println("Год издания: ");
+        hl.year_of_publication = new Scanner(System.in).nextInt();
+
+        System.out.println("Издательство: ");
+        hl.publiching_house = new Scanner(System.in).nextLine();
+
+        System.out.println("Обложка(Переплёт)");
+        hl.crust = new Scanner(System.in).nextLine();
+
+      /*  System.out.print("Имя: ");
         System.out.println(av.first_name);
 
         System.out.print("Фамилия: ");
         System.out.println(av.last_name);
 
         System.out.print("Отчество: ");
-        System.out.print(av.middle_name);
-
+        System.out.print(av.middle_name);*/
 
         //запись в файл
         FileWriter fw = new FileWriter("src\\out\\test.txt", true); //фальш на перезапись
@@ -38,6 +55,14 @@ public class Main {
             fw.write("Имя: " + av.first_name + "\n");
             fw.write("Фамилия: " + av.last_name + "\n");
             fw.write("Отчество: " + av.middle_name + "\n");
+
+            fw.write("Жанр: " + hl.genre + "\n");
+            fw.write("Количество страниц: " + hl.number_of_pages + "\n");
+            fw.write("Год издания: " + hl.year_of_publication + "\n");
+            fw.write("Издательство: " + hl.publiching_house + "\n");
+
+            fw.write("Обложка/Переплёт: "+hl.crust+"\n");
+
             fw.write("------------------" + "\n");
         } finally {
             fw.close();
