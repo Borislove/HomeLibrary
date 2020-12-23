9757a5e6f845fd84b488e8b352187646775e620b
Borislove <abirme@yandex.ru>
2020-12-24 01:16:58 +0300

    файл под запись

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
     //public static String sc = new Scanner(System.in).nextLine();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //new Home_Library("Pupkin", "Vasya", "Sergeevich", "philosophy", 777, 2011, "Dialectic");
         //    System.out.println(new Home_Library("Pupkin", "Vasya", "Sergeevich", "philosophy", 777, 2011, "Dialectic"));
 
@@ -18,7 +20,7 @@ public class Main {
         av.middle_name = new Scanner(System.in).nextLine();
 
 
-        System.out.print ("Имя: ");
+        System.out.print("Имя: ");
         System.out.println(av.first_name);
 
         System.out.print("Фамилия: ");
@@ -26,5 +28,19 @@ public class Main {
 
         System.out.print("Отчество: ");
         System.out.print(av.middle_name);
+
+
+        //запись в файл
+        FileWriter fw = new FileWriter("test", true);
+
+        try {
+            fw.write("------------------" + "\n");
+            fw.write("Имя: " + av.first_name + "\n");
+            fw.write("Фмилия: " + av.last_name + "\n");
+            fw.write("Отчество: " + av.middle_name + "\n");
+            fw.write("------------------" + "\n");
+        } finally {
+            fw.close();
+        }
     }
 }
