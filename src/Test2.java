0c7515779f5b54032874f56deddf844b76ad42b9
Borislove <abirme@yandex.ru>
2020-12-29 14:04:34 +0300

    test2

---
+++ src/Test2.java
@@ -0,0 +1,29 @@
+public class Test2 {
+
+    private static String last_name; //фамилия
+    private static String first_name = ""; //имя
+    private static String middle_name; //отчество?
+
+
+    Test2(String last_name, String first_name, String middle_name) {
+        this.last_name = last_name;
+        this.first_name = first_name;
+        this.middle_name = middle_name;
+    }
+
+    public static void main(String[] args) {
+        Test2 t = new Test2("Vasilev", "Serg", "Sergeevich");
+        /*t.last_name = "last_name";
+        t.first_name = "first_name";
+        t.middle_name = "middle_name";*/
+        System.out.println(t.toString()); //+
+        System.out.println(t); //+
+    }
+
+    @Override
+    public String toString() {
+        return last_name +" "+first_name+" "+middle_name;
+    }
+}
+
+
